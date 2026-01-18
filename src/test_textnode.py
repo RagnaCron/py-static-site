d641138277cf52b4d51c071971313707a48be148
Manuel Werder <manuelwerder@hotmail.com>
2026-01-18 16:14:13 +0100

    Added test case for textnode

---
+++ src/test_textnode.py
@@ -0,0 +1,34 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq_link(self):
+        node = TextNode("This is a link node", TextType.LINK, "https://www.google.com")
+        node2 = TextNode("This is a link node", TextType.LINK, "https://www.google.com")
+        self.assertEqual(node, node2)
+
+    def test_not_eq_diff_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is another text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_diff_types(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node3 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node3)
+
+    def test_not_eq_none_url(self):
+        node = TextNode("This is a link node", TextType.LINK, "https://www.google.com")
+        node2 = TextNode("This is a link node", TextType.LINK, None)
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
